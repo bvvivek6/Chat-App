69749fae537016c8b2bce6cdc7fa3195877a9583
bvvivek6 <vivekbv242813@gmail.com>
2025-03-23 21:08:41 +0530

    add profile page and UI fixes

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./RightSideBar.css";
+import assets from "../../assets/assets";
 
 const RightSideBar = () => {
-  return <div>RightSideBar</div>;
+  return (
+    <div className="rs">
+      <div className="rs-rpofile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Vicky
+          <img src={assets.green_dot} />
+        </h3>
+        <p>Hawk tua, you gotta split on that thing!</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideBar;
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>We are om the Profile page</div>;
+  return (
+    <div className="pr-container">
+      <form>
+        <h3>Profile Details</h3>
+        <label htmlFor="avatar">
+          <input type="file" accept=".png, .jpeg, .jpg" hidden />
+          <img src={assets.avatar_icon} alt="" />
+          Upload your profile image
+        </label>
+        <input type="text" placeholder="Your name" required />
+        <textarea placeholder="write a bio" required />
+        <button>Save</button>
+      </form>
+    </div>
+  );
 };
 
 export default ProfileUpdate;

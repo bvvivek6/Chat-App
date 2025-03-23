2ca19412e2e05bac6648012b520a1b2be6e516e7
bvvivek6 <vivekbv242813@gmail.com>
2025-03-23 21:20:22 +0530

    fix: resolve merge conflict in RightSideBar component and clean up profile section

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -5,30 +5,28 @@ import assets from "../../assets/assets";
 const RightSideBar = () => {
   return (
     <div className="rs">
-<<<<<<< HEAD
       <div className="rs-rpofile">
-=======
-      <div className="rs-profile">
->>>>>>> c00d6ce58353b83eadd0fa5b360499168a00e9a2
-        <img src={assets.profile_img} alt="" />
-        <h3>
-          Vicky
-          <img src={assets.green_dot} />
-        </h3>
-        <p>Hawk tua, you gotta split on that thing!</p>
-      </div>
-      <hr />
-      <div className="rs-media">
-        <p>Media</p>
-        <div>
-          <img src={assets.pic1} alt="" />
-          <img src={assets.pic2} alt="" />
-          <img src={assets.pic3} alt="" />
-          <img src={assets.pic4} alt="" />
-          <img src={assets.pic1} alt="" />
-          <img src={assets.pic2} alt="" />
+        <div className="rs-profile">
+          <img src={assets.profile_img} alt="" />
+          <h3>
+            Vicky
+            <img src={assets.green_dot} />
+          </h3>
+          <p>Hawk tua, you gotta split on that thing!</p>
         </div>
-        <button>Logout</button>
+        <hr />
+        <div className="rs-media">
+          <p>Media</p>
+          <div>
+            <img src={assets.pic1} alt="" />
+            <img src={assets.pic2} alt="" />
+            <img src={assets.pic3} alt="" />
+            <img src={assets.pic4} alt="" />
+            <img src={assets.pic1} alt="" />
+            <img src={assets.pic2} alt="" />
+          </div>
+          <button>Logout</button>
+        </div>
       </div>
     </div>
   );
